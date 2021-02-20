fba5d48372d706646da06d63fdee358fb9c658a2
pavellyush <lush.pavel@gmail.com>
2021-02-20 18:12:04 +0200

    -

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/AsteroidListItemDescriptions.kt
@@ -0,0 +1,7 @@
+package com.udacity.asteroidradar
+
+class AsteroidListItemDescriptions(
+    val codeNameDesc: String,
+    val closeApproachDateDesc: String,
+    val isPotentiallyHazardousDesc: String
+)
